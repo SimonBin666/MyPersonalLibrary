ce29cfe76abf12720c27eb5970dc48727276ee1a
simon <1423308710@qq.com>
2022-11-16 19:51:52 +0800

    完成浮动操作菜单

---
+++ app/src/main/java/com/example/mypersonallibrary/Book.java
@@ -0,0 +1,10 @@
+package com.example.mypersonallibrary;
+import java.util.Date;
+public class Book {
+    private int isbn;
+    private String title;
+    private String[] authors;
+    private String[] translators;
+    private String publisher;
+    private Date pubtime;
+}
